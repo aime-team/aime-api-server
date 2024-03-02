4a5bec8ce285ef97d0b9423d81024070392350d1
toine <toine@aime.info>
2024-03-02 21:33:46 +0100

    improved job_batch processing

--- endpoints/example_api/example_batch_worker.py
+++ endpoints/example_api/example_batch_worker.py
@@ -7,7 +7,7 @@ WORKER_JOB_SERVER = 'http://localhost:7777'
 WORKER_JOB_TYPE = "example_worker"
 WORKER_AUTH_KEY = "869a93805a301f8a41e0610129a72b19"
 
-MAX_JOB_BATCH = 2
+MAX_JOB_BATCH = 3
 
 
 FRAME_RATE = 150
@@ -17,7 +17,7 @@ def main():
     api_worker = APIWorkerInterface(WORKER_JOB_SERVER, WORKER_JOB_TYPE, WORKER_AUTH_KEY)
 
     while True:
-        print('--- waiting for jobs of type: ' + WORKER_JOB_TYPE)
+        print('--- waiting for jobs of type: ' + WORKER_JOB_TYPE + ' - batch size: ' + str(MAX_JOB_BATCH))
         job_batch_data = api_worker.job_batch_request(max_job_batch=MAX_JOB_BATCH)
         for job_data in job_batch_data:
             print('- received job: ', job_data)
@@ -25,7 +25,6 @@ def main():
         job_step_counter = [0] * MAX_JOB_BATCH
         job_step_duration = [0] * MAX_JOB_BATCH
         job_sleep_counter = [0] * MAX_JOB_BATCH
-        job_finished = [False] * MAX_JOB_BATCH
 
         for idx, job_data in enumerate(job_batch_data):
             sleep_duration = float(job_data['sleep_duration'])
@@ -34,18 +33,15 @@ def main():
             if(progress_steps > 0):
                 job_step_duration[idx] = (FRAME_RATE * sleep_duration) / progress_steps
 
-        all_jobs_finished = False
-
-        while not all_jobs_finished:
+        while not api_worker.have_all_jobs_finished():
             for idx, job_data in enumerate(job_batch_data):
-                if job_finished[idx]:
+                if api_worker.has_job_finished(job_data):
                     continue
 
                 if job_sleep_counter[idx] >= job_step_duration[idx]:
                     progress_steps = int(job_data['progress_steps'])
                     if job_step_counter[idx] >= progress_steps:
                         # job finished
-                        job_finished[idx] = True
                         print(job_data['job_id'] + ": finished.")
 
                         results = {}
@@ -66,13 +62,6 @@ def main():
                     job_sleep_counter[idx] += 1
 
             time.sleep(1.0 / FRAME_RATE)   # simulate work
-
-            # check if all jobs have finished
-            all_jobs_finished = True
-            for idx in range(len(job_batch_data)):
-                if not job_finished[idx]:
-                    all_jobs_finished = False
-                    break
 
 
 
