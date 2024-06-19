12fda184f65b4d8e61779e75b95cb364f373dbb6
headkit <henri@headkit-studio.de>
2024-06-19 18:58:44 +0200

    pimping llama3 UX/UI

--- endpoints/llama3_chat/static/js/main.js
+++ endpoints/llama3_chat/static/js/main.js
@@ -315,13 +315,13 @@ function addChatboxBubble(chatText, infoDetails, isResponse = false, editable=fa
             <div class="flex items-start gap-2.5">
                 <div class="flex flex-col gap-1 w-full max-w-[320px]">
                     <div class="flex items-center justify-between rtl:justify-end space-x-2">
-                        <span class="text-sm font-semibold text-white">
-                        ${isResponse ? '<img src="/mixtral-chat/static/mixtral_icon.png" style="width: 20px; height: 20px;">' : '<img src="/mixtral-chat/static/user_icon.png" style="width: 20px; height: 20px;">'}
-                        </span>
-                        <span class="overlook text-xs font-normal text-gray-500 text-gray-400 ml-auto">${localISOTime.match(/\d\d:\d\d/)}</span>
+                        ${isResponse ? '<img src="/llama3-chat/static/img/llama_icon.png" style="width: 12px; height: 12px;">' : ''}
+                        <span class="overlook text-xs font-normal text-gray-500 text-gray-400">${localISOTime.match(/\d\d:\d\d/)}</span>
+                        ${!isResponse ? '<span class="text-sm font-semibold text-white w-4 h-4"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="#fff" class="size-4"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg></span>' : ''}
                     </div>
                     <div class="flex flex-col leading-1.5 p-4 border-gray-200 bg-gray-600 rounded-xl ${!isResponse ? 'rounded-br-none' : 'rounded-tl-none'}">
                         <div contenteditable="${editable}" class="eol-node ${!isResponse ? 'user-bubble' : 'assistant-bubble'} latest-bubble-text text-sm font-normal text-white">${chatText}</div>
+                        ${!editable ? '' : '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#999" class="size-6 w-4 h-4"><path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" /><path d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z" /></svg>'}
                     </div>
                     <span class="overlook latest-bubble-info text-xs font-normal text-gray-400">${infoDetails}</span>
                 </div>
