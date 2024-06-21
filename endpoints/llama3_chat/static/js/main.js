661fed900f1553c8fb99da1cc290e24e1ebef5b9
carlo <carlo@aime.info>
2024-06-21 12:52:41 +0200

    Llama 3 + mixtral demo main.js: Chatbubble with id 'latest-bubble-text' now loads chatText with method innerContent() to avoid html interpretation of the text string

--- endpoints/llama3_chat/static/js/main.js
+++ endpoints/llama3_chat/static/js/main.js
@@ -361,14 +361,14 @@ function addChatboxBubble(chatText, infoDetails, isResponse = false, editable=fa
                         ${!isResponse ? '<span class="text-sm font-semibold text-white w-4 h-4"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="#fff" class="size-4"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg></span>' : ''}
                     </div>
                     <div class="flex flex-col leading-1.5 p-4 border-gray-200 bg-gray-600 rounded-xl ${!isResponse ? 'rounded-br-none' : 'rounded-tl-none'}">
-                        <div contenteditable="${editable}" class="eol-node ${!isResponse ? 'user-bubble' : 'assistant-bubble'} latest-bubble-text text-sm font-normal text-white">${chatText}</div>
+                        <div contenteditable="${editable}" class="eol-node ${!isResponse ? 'user-bubble' : 'assistant-bubble'} latest-bubble-text text-sm font-normal text-white"></div>
                         ${!editable ? '' : '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#999" class="size-6 w-4 h-4"><path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" /><path d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z" /></svg>'}
                     </div>
                     <span class="overlook latest-bubble-info text-xs font-normal text-gray-400">${infoDetails}</span>
                 </div>
             </div>
     `;
-
+    chatBubbleEl.querySelector('.latest-bubble-text').textContent = chatText;
     chatBubbleEl.classList.add(!isResponse ? 'justify-end' : 'justify-start');
     chatBubbleEl.querySelector('.flex').classList.add(!isResponse ? 'flex-row-reverse' : 'flex-row');
     
