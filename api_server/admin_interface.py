b942d6c7a20943263ca25b2031e99980f562ff69
toine <toine@aime.info>
2025-03-06 12:00:53 +0100

    merged admin_interface from admin and main branch

--- api_server/admin_interface.py
+++ api_server/admin_interface.py
@@ -1,12 +1,12 @@
-from .__version import __version__
 
+from .__version import __version__
 
 class AdminInterface():
     """
     AIME API Server AdminInterface provided by API Server
 
     Args:
-        sanic (Sanic): Instance of Sanic server
+        app (Sanic): Instance of Sanic server
         args: command line arguments that where passed
         server_configuration: JSON of loaded server configuration
 
@@ -16,36 +16,36 @@ class AdminInterface():
         - no python objects: only strings, int, boolean, float and dict and arrays will be passed
         - is_ do_ methods are calls from API Server -> Admim BE
     """
-    def __init__(self, sanic, args, server_configuration):
+    def __init__(self, app, args, server_configuration):
         # overwritten and implemented by Admin BE
-        self.sanic = sanic
+        self.app = app
 
 
-    #### user authentification / authorization
+    # user authentification / authorization
     # 
-    async def admin_is_user_known(self, user_name):
+    async def admin_is_user_authenticated(self, request):
         # implemented by Admin BE
         pass
 
-    async def admin_do_log_in_user(self, user_name, password_hash):
+    async def admin_do_log_in_user(self, request):       # ---> request obj. should have json (email, password)
         # implemented by Admin BE
         return False
 
-    async def admin_do_log_out_user(self, user_name):
+    async def admin_do_log_out_user(self, request):
         # implemented by Admin BE
         return True
 
-    async def admin_is_user_authorized_for_basic_operations(self, user_name, auth_key):
+    async def admin_is_user_authorized_for_basic_operations(self, user_email, auth_key):
         # implemented by Admin BE        
         return False
 
-    async def admin_is_user_authorized_for_endpoint(self, user_name, endpoint_name, auth_key):
+    async def admin_is_user_authorized_for_endpoint(self, user_email, endpoint_name, auth_key):
         # implemented by Admin BE        
         return False
 
 
     # API Key Managment
-    async def admin_is_api_key_valid(self, user_name, api_key, endpoint):
+    async def admin_is_api_key_valid(self, user_email, api_key, endpoint):
         # implemented by Admin BE
         return False
 
@@ -56,10 +56,11 @@ class AdminInterface():
         Returns:
             list of strings: List of known endpoints
         """
-        return [endpoint_name for endpoint_name in self.sanic.endpoints.keys()]
+        return [endpoint_name for endpoint_name in self.app.endpoints.keys()]
 
 
-    async def api_endpoint_status(self, endpoint_name):
+
+    async def api_get_endpoint_status(self, endpoint_name):
         """_summary_
 
         Args:
@@ -69,10 +70,10 @@ class AdminInterface():
             dict: Dictionary with endpoint status
         """        
 
-        endpoint = self.sanic.endpoints.get(endpoint_name)
-        job_type = self.sanic.job_handler.job_types.get(endpoint.worker_job_type)
+        endpoint = self.app.endpoints.get(endpoint_name)
+        job_type = self.app.job_handler.job_types.get(endpoint.worker_job_type)
         if job_type and endpoint:
-            ep_status = endpoint.status_data
+            ep_status = endpoint.status_data_sync
             ep_status.update({
                 'num_workers_online': await job_type.get_num_workers_online(),
                 'num_processing_requests': job_type.get_num_running_jobs(endpoint_name),
@@ -111,18 +112,18 @@ class AdminInterface():
 
 
     async def api_set_endpoint_enable(self, endpoint_name):
-        endpoint = self.sanic.endpoints.get(endpoint_name)
+        endpoint = self.app.endpoints.get(endpoint_name)
         if endpoint:
             endpoint.enable()
 
     async def api_set_endpoint_disable(self, endpoint_name):
-        endpoint = self.sanic.endpoints.get(endpoint_name)
+        endpoint = self.app.endpoints.get(endpoint_name)
         if endpoint:
             endpoint.disable()
 
 
     async def api_get_endpoint_config(self, endpoint_name):
-        endpoint = self.sanic.endpoints.get(endpoint_name)
+        endpoint = self.app.endpoints.get(endpoint_name)
         if endpoint:
             return endpoint.config
 
@@ -151,22 +152,22 @@ class AdminInterface():
 
 
     async def api_get_worker_list(self):
-        return await self.sanic.job_handler.get_all_workers()
+        return await self.app.job_handler.get_all_workers()
 
 
     async def api_get_active_worker_list(self):
-        return await self.sanic.job_handler.get_all_active_workers()
+        return await self.app.job_handler.get_all_active_workers()
 
 
     async def api_get_worker_status(self, worker_auth):
-        status = await self.sanic.job_handler.get_worker_state(worker_auth)
+        status = await self.app.job_handler.get_worker_state(worker_auth)
         if status:
             return status.value
 
 
     async def admin_set_worker_online(self, worker_auth):
-        return await self.sanic.job_handler.activate_worker(worker_auth)
+        return await self.app.job_handler.activate_worker(worker_auth)
 
 
     async def admin_set_worker_offline(self, worker_auth):
-        return await self.sanic.job_handler.deactivate_worker(worker_auth)
+        return await self.app.job_handler.deactivate_worker(worker_auth)
