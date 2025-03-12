2fee768d0a6b03eba31971f7c4c0c362cb6a40cf
carlo_vogel <carlo@aime.info>
2025-03-12 11:36:31 +0100

    Changed Methods of AdminInterface() as discussed in Dev-Meeting, implemented MinimumAdminBackendImplementation()

--- api_server/admin_interface.py
+++ api_server/admin_interface.py
@@ -1,5 +1,5 @@
-
 from .__version import __version__
+from .utils.misc import get_job_counter_id, shorten_strings
 
 class AdminInterface():
     """
@@ -21,50 +21,38 @@ class AdminInterface():
         self.app = app
 
 
-    # user authentification / authorization
+    # API key authorization
     # 
-    async def admin_is_user_authenticated(self, request):
-        # implemented by Admin BE
-        pass
-
-    async def admin_do_log_in_user(self, request):       # ---> request obj. should have json (email, password)
-        # implemented by Admin BE
-        return False
-
-    async def admin_do_log_out_user(self, request):
-        # implemented by Admin BE
-        return True
-
-    async def admin_is_user_authorized_for_basic_operations(self, user_email, auth_key):
-        # implemented by Admin BE        
-        return False
-
-    async def admin_is_user_authorized_for_endpoint(self, user_email, endpoint_name, auth_key):
+    async def admin_is_api_key_authorized_for_endpoint(self, api_key, endpoint_name):
         # implemented by Admin BE        
         return False
 
 
-    # API Key Managment
-    async def admin_is_api_key_valid(self, user_email, api_key, endpoint):
+    # API Key Management
+    async def admin_is_api_key_valid(self, api_key):
         # implemented by Admin BE
-        return False
+        return {
+            'valid': True,
+            'error_msg': None
+            }
+
 
 
     async def api_get_endpoint_list(self):
         """Retrieve all endpoints known to the API Server.
 
         Returns:
-            list of strings: List of known endpoints
+            list[str]: List of known endpoints
         """
         return [endpoint_name for endpoint_name in self.app.endpoints.keys()]
 
 
 
     async def api_get_endpoint_status(self, endpoint_name):
-        """_summary_
+        """Retrieve 
 
         Args:
-            endpoint_name (_type_): _description_
+            endpoint_name (str): Name of the endpoint
 
         Returns:
             dict: Dictionary with endpoint status
@@ -99,18 +87,47 @@ class AdminInterface():
         pass
 
 
-    async def api_set_endpoint_enable(self, endpoint_name):
+    async def api_set_endpoint_enabled(self, endpoint_name):
+        """Enable endpoint with given endpoint name
+
+        Args:
+            endpoint_name (str): Name of the endpoint
+
+        Returns:
+            bool: True if endpoint_name is present, False otherwise
+        """        
         endpoint = self.app.endpoints.get(endpoint_name)
         if endpoint:
             endpoint.enable()
+            return True
+        else:
+            return False
 
-    async def api_set_endpoint_disable(self, endpoint_name):
+    async def api_set_endpoint_disabled(self, endpoint_name):
+        """Disable endpoint with given endpoint name
+
+        Args:
+            endpoint_name (str): Name of the endpoint
+
+        Returns:
+            bool: True if endpoint_name is present, False otherwise
+        """    
         endpoint = self.app.endpoints.get(endpoint_name)
         if endpoint:
             endpoint.disable()
-
+            return True
+        else:
+            return False
 
     async def api_get_endpoint_config(self, endpoint_name):
+        """Retrieve endpoint configuration of given endpoint name
+
+        Args:
+            endpoint_name (str): Name of the endpoint
+
+        Returns:
+            dict: Endpoint configuration
+        """        
         endpoint = self.app.endpoints.get(endpoint_name)
         if endpoint:
             return endpoint.config
@@ -125,20 +142,18 @@ class AdminInterface():
     #
     async def admin_log_request_start(
         self,
-        ip_address,
-        job_hash_key,
         job_id,
-        user_email,
+        api_key,
         endpoint_name,
-        request_header,
-        start_time,
-        start_time_compute
+        start_time_utc,
+        ip_address=None, 
+        http_request_header:dict=None
         ):
         # Implemented by Admin BE
         pass
 
 
-    async def admin_log_request_processing(self, endpoint_name, user, job_id):
+    async def admin_log_request_start_processing(self, job_id, start_time_compute_utc):
         # Implemented by Admin BE
         pass
 
@@ -146,31 +161,131 @@ class AdminInterface():
     async def admin_log_request_end(
         self,
         job_id,
-        request_status,
-        end_datetime
+        end_time_utc,
+        request_state, # 'success', 'failed', 'canceled' ,
+        request_error_msg=None
         ):
         # Implemented by Admin BE
-        # request_status: "error", "success"
         pass
 
 
     async def api_get_worker_list(self):
+        """Retrieve list of all worker names.
+
+        Returns:
+            list[str]: List of workers
+        """        
         return await self.app.job_handler.get_all_workers()
 
 
-    async def api_get_active_worker_list(self):
-        return await self.app.job_handler.get_all_active_workers()
+    async def api_get_worker_config(self, worker_name):
+        """Retrieve configuration of worker with given worker name.
+
+        Args:
+            worker_name (str): Name of the worker
+
+        Returns:
+            dict: Worker configuration containing job_type, worker_auth_key, etc.
+        """        
+        return self.app.job_handler.get_worker_config(worker_name)
 
 
-    async def api_get_worker_status(self, worker_auth):
-        status = await self.app.job_handler.get_worker_state(worker_auth)
+    async def api_get_worker_status(self, worker_name):
+        """Retrieve the current status of the worker with given worker name.
+
+        Args:
+            worker_name (str): Name of the worker
+
+        Returns:
+            str: Current worker status: 'processing', 'waiting', 'offline' or 'disabled'
+        """        
+        status = await self.app.job_handler.get_worker_state(worker_name)
         if status:
             return status.value
 
 
-    async def admin_set_worker_online(self, worker_auth):
-        return await self.app.job_handler.activate_worker(worker_auth)
+    async def admin_set_worker_enabled(self, worker_name):
+        """Enable worker with given worker name
+
+        Args:
+            worker_name (str): Name of the worker
+
+        Returns:
+            bool: True if worker_name is present, False otherwise
+        """        
+        return await self.app.job_handler.enable_worker(worker_name)
 
 
-    async def admin_set_worker_offline(self, worker_auth):
-        return await self.app.job_handler.deactivate_worker(worker_auth)
+    async def admin_set_worker_disabled(self, worker_name):
+        """Disable worker with given worker name
+
+        Args:
+            worker_name (str): Name of the worker
+
+        Returns:
+            bool: True if worker_name is present, False otherwise
+        """            
+        return await self.app.job_handler.disable_worker(worker_name)
+
+
+
+class MinimumAdminBackendImplementation(AdminInterface):
+
+
+    async def admin_is_api_key_authorized_for_endpoint(self, api_key, endpoint_name):
+        endpoint_config = await self.api_get_endpoint_config(endpoint_name)
+        return True if api_key in endpoint_config.get('CLIENTS', {}).get('authorization_keys').values() else False
+    
+
+    # API Key Management
+    async def admin_is_api_key_valid(self, api_key):
+        for endpoint_name in await self.api_get_endpoint_list():
+            if await self.admin_is_api_key_authorized_for_endpoint(api_key, endpoint_name):
+                return {
+                    'valid': True,
+                    'error_msg': None
+                }
+        return {
+            'valid': False,
+            'error_msg': 'Key not valid'
+        }
+
+
+    async def admin_log_request_start(
+        self,
+        job_id:str,
+        api_key:str,
+        endpoint_name:str,
+        start_time_utc:float,
+        ip_address:str=None,
+        http_request_header:dict=None
+        ):
+        self.app.logger.debug(
+            f'Admin Backend call to admin_log_request_start from '
+            f'job: {get_job_counter_id(job_id)}, '
+            f'endpoint_name: {endpoint_name}, '
+            f'ip_address: {ip_address}, '
+            f'header: {http_request_header}'
+            )
+
+
+    async def admin_log_request_start_processing(self, job_id, start_time_compute_utc):
+        self.app.logger.debug(
+            f'Admin Backend call to admin_log_request_start_processing from job: {get_job_counter_id(job_id)}'
+        )
+
+
+    async def admin_log_request_end(
+        self,
+        job_id,
+        end_time_utc,
+        request_state, # 'success', 'failed', 'canceled'
+        request_error_msg=None
+        ):
+        self.app.logger.debug(
+            f'Admin Backend call to admin_log_request_end from '
+            f'job: {get_job_counter_id(job_id)}, '
+            f'end_datetime: {end_time_utc}, '
+            f'request_state: {request_state}, '
+            f'request_error_msg: {request_error_msg}'
+        )
